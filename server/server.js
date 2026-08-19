3472ca5444a2acf9acbb9df0697ae3d4db3495b1
sesan-7 <senabadhy.nrc07@gmail.com>
2026-08-19 18:15:26 +0530

    client and server url fetched

--- server/server.js
+++ server/server.js
@@ -12,7 +12,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
